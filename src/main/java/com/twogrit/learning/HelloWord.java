11a5cbb6cf7f21f220c96ba8501b3f734de6b1b9
ke.zhang <ke.zhang@alipay.com>
2016-08-11 16:39:55 +0800

    hello word

---
+++ src/main/java/com/twogrit/learning/HelloWord.java
@@ -0,0 +1,7 @@
+package com.twogrit.learning;
+
+public class HelloWord {
+    public String  sayHello(String name) {
+        return "Hello "+name;
+    }
+}
---
+++ src/test/java/com/twogrit/learning/HelloWordTest.java
@@ -0,0 +1,15 @@
+package com.twogrit.learning;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWordTest {
+    private HelloWord helloWord=new HelloWord();
+
+    @Test
+    public void shouldSayHello(){
+        Assert.assertEquals("Hello yexueren01", helloWord.sayHello("yexueren01"));
+
+    }
+
+}
