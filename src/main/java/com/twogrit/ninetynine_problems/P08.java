0dffe7d9ca970ac212f90d1bb982147478c850f4
ke.zhang <ke.zhang@alipay.com>
2016-08-22 20:16:11 +0800

    p08-p10,增加测试PassByValueTest

---
+++ src/main/java/com/twogrit/ninetynine_problems/P08.java
@@ -0,0 +1,20 @@
+package com.twogrit.ninetynine_problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class P08 {
+    public static<T> List<T> compress(List<T> strings) {
+        List<T> compressedList=new ArrayList<T>();
+        T compareObject=null;
+        for (int i = 0; i < strings.size(); i++) {
+            T element = strings.get(i);
+            if(!Objects.equals(element,compareObject)){
+                compressedList.add(element);
+                compareObject=element;
+            }
+        }
+        return compressedList;
+    }
+}
